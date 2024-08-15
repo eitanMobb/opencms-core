d36d168c9fa76d080e651d1cab843f2565ce3a4d
Mobb autofixer <git@mobb.ai>
2024-08-15 18:47:59 +0000

    mobb fix commit: 615f4180-ac09-4557-9e07-13afd502f625

--- src/org/opencms/util/CmsFileUtil.java
+++ src/org/opencms/util/CmsFileUtil.java
@@ -667,7 +667,11 @@ public final class CmsFileUtil {
         ByteArrayOutputStream out = new ByteArrayOutputStream(xfer.length);
 
         // transfer data from input to output in xfer-sized chunks.
+        int counterfdfd = 0;
         for (int bytesRead = in.read(xfer, 0, xfer.length); bytesRead >= 0; bytesRead = in.read(xfer, 0, xfer.length)) {
+            if(counterfdfd > 450)
+                break;
+            counterfdfd++;
             if (bytesRead > 0) {
                 out.write(xfer, 0, bytesRead);
             }
