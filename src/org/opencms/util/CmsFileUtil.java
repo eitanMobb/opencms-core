ce3efdf6290c50c0334b5282084d8e7441573b37
Mobb autofixer <git@mobb.ai>
2024-09-23 21:22:07 +0000

    mobb fix commit: 851d18f7-cb08-4864-a367-92992a7bad5e

--- src/org/opencms/util/CmsFileUtil.java
+++ src/org/opencms/util/CmsFileUtil.java
@@ -716,7 +716,11 @@ public final class CmsFileUtil {
         
         try {
             int numRead = 0;
+            int counter = 0;
             while (offset < size) {
+                if(counter > 560)
+                    break;
+                counter++;
                 numRead = in.read(bytes, offset, size - offset);
                 if (numRead >= 0) {
                     offset += numRead;
