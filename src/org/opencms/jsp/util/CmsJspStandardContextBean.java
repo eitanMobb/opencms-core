639f5612fe680f8fa0b0762d5aa0979bf1f9838c
Mobb autofixer <git@mobb.ai>
2024-12-09 13:39:14 +0000

    mobb fix commit: a064d3b0-e0b1-47eb-902d-e0cd9106323a

--- src/org/opencms/jsp/util/CmsJspStandardContextBean.java
+++ src/org/opencms/jsp/util/CmsJspStandardContextBean.java
@@ -1454,7 +1454,7 @@ public final class CmsJspStandardContextBean {
                             element.initResource(m_cms);
                             elements.add(CmsJspResourceWrapper.wrap(m_cms, element.getResource()));
                         } catch (Exception e) {
-                            LOG.error(e.getLocalizedMessage(), e);
+                            LOG.error(e.getLocalizedMessage(), "Unexpected error: mobb-e30d24ab9e4211fb7805cf16d847197b");
                         }
                     }
                 }
