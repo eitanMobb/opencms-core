7fb9525e645eaa6d6e9025281c80ee82039449a3
Mobb autofixer <git@mobb.ai>
2024-12-09 13:37:33 +0000

    mobb fix commit: 8de95787-17cb-4080-9861-227a3593c42c

--- src/org/opencms/ade/detailpage/CmsDetailPageFilter.java
+++ src/org/opencms/ade/detailpage/CmsDetailPageFilter.java
@@ -86,7 +86,7 @@ public class CmsDetailPageFilter {
             m_resource = resource;
         } catch (CmsException e) {
             // shouldn't happen - initCmsObject doesn't *actually* throw exceptions
-            LOG.error(e.getLocalizedMessage(), e);
+            LOG.error("Unexpected error: mobb-2cab960332f060828cf3bbb0932cc06b", e);
         }
 
     }
